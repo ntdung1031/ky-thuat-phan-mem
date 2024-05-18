97a94da0f67827e0396ae277c6c7c852062f5339
Nguyen Tri Dung <ntdung1031>
2024-05-18 23:30:00 +0700

    form sign in and form sign up

--- js/js.js
+++ js/js.js
@@ -71,3 +71,59 @@ form_thanks.addEventListener('submit',function() {
     thanks.classList.add('open')
 })
 
+// form
+var html = document.querySelector(html)
+var signUp = document.querySelector('.sign-up')
+var signIn = document.querySelector('.sign-in')
+var modal = document.getElementsByClassName('modal')
+var modalBody = document.getElementsByClassName('modal-body')
+var signInMore = document.querySelectorAll('.dont-SignIn > a')
+var closeForm = document.getElementsByClassName('close-form')
+for(let i of modal ) {
+    signIn.addEventListener('click',() => {
+        modal[0].style.display = 'block'
+        modal[1].style.display = 'none'
+    })
+    signUp.addEventListener('click',() => {
+        modal[0].style.display = 'none'
+        modal[1].style.display = 'block'
+    })  
+}
+
+modal[0].addEventListener('click',(e) => {
+    modal[0].style.display = 'none'
+})
+modal[1].addEventListener('click',(e) => {
+    modal[1].style.display = 'none'
+})
+
+modalBody[0].addEventListener('click',(e) => {
+    e.stopPropagation()
+})
+modalBody[1].addEventListener('click',(e) => {
+    e.stopPropagation()
+})
+signInMore[0].addEventListener('click', () => {
+    modal[0].style.display = 'none'
+    modal[1].style.display = 'block'
+})
+signInMore[1].addEventListener('click', () => {
+    modal[0].style.display = 'block'
+    modal[1].style.display = 'none'
+})
+
+closeForm[0].addEventListener('click', () => {
+    modal[0].style.display = 'none'
+    modal[1].style.display = 'none'
+})
+closeForm[1].addEventListener('click', () => {
+    modal[0].style.display = 'none'
+    modal[1].style.display = 'none'
+})
+
+
+
+
+
+
+
